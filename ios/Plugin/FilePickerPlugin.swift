8b2c4b51a0bc770d122683e1ba6011089e903d9d
Robin Genz <mail@robingenz.dev>
2021-08-14 10:25:25 +0200

    wip

--- ios/Plugin/FilePickerPlugin.swift
+++ ios/Plugin/FilePickerPlugin.swift
@@ -7,9 +7,38 @@ import Capacitor
  */
 @objc(FilePickerPlugin)
 public class FilePickerPlugin: CAPPlugin {
-    private let implementation = FilePicker()
+    public let errorPickFileCanceled = "pickFile canceled."
+    private var implementation: FilePicker?;
+    private var savedCall: CAPPluginCall?
+    
+    override public func load() {
+        self.implementation = FilePicker(self)
+    }
 
     @objc func pickFile(_ call: CAPPluginCall) {
-        call.reject("Not implemented on iOS.")
+        savedCall = call
+        implementation?.openDocumentPicker()
+    }
+    
+    func handleDocumentPickerResult(url: URL?) {
+        guard let savedCall = savedCall else {
+            return
+        }
+        guard let url = url else {
+            savedCall.reject(errorPickFileCanceled)
+            return
+        }
+        do {
+            savedCall.resolve([
+                "path": implementation?.getPathFromUrl(url) ?? "",
+                "name": implementation?.getNameFromUrl(url) ?? "",
+                "data": try implementation?.getDataFromUrl(url) ?? "",
+                "mimeType": implementation?.getMimeTypeFromUrl(url) ?? "",
+                "size": try implementation?.getSizeFromUrl(url) ?? ""
+            ])
+        } catch let error as NSError {
+            savedCall.reject(error.localizedDescription, nil, error)
+            return
+        }
     }
 }
