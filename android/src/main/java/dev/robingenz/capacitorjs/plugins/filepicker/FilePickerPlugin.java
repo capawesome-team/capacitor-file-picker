929f3fbac9bf856f1f8c7bca134fd8ab939d1913
Robin Genz <mail@robingenz.dev>
2021-08-29 11:12:02 +0200

    wip: android

--- android/src/main/java/dev/robingenz/capacitorjs/plugins/filepicker/FilePickerPlugin.java
+++ android/src/main/java/dev/robingenz/capacitorjs/plugins/filepicker/FilePickerPlugin.java
@@ -4,12 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import androidx.activity.result.ActivityResult;
+import androidx.annotation.Nullable;
+import com.getcapacitor.JSArray;
 import com.getcapacitor.JSObject;
+import com.getcapacitor.Logger;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import java.util.List;
+import org.json.JSONException;
 
 @CapacitorPlugin(name = "FilePicker")
 public class FilePickerPlugin extends Plugin {
@@ -24,13 +29,33 @@ public class FilePickerPlugin extends Plugin {
 
     @PluginMethod
     public void pickFile(PluginCall call) {
+        JSArray types = call.getArray("types");
+        String[] parsedTypes = parseTypesOption(types);
+
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
+        if (parsedTypes != null) {
+            intent.putExtra(Intent.EXTRA_MIME_TYPES, parsedTypes);
+        }
 
         intent = Intent.createChooser(intent, "Choose a file");
 
         startActivityForResult(call, intent, "pickFileResult");
+    }
+
+    @Nullable
+    private String[] parseTypesOption(@Nullable JSArray types) {
+        if (types == null) {
+            return null;
+        }
+        try {
+            List<String> typesList = types.toList();
+            return typesList.toArray(new String[0]);
+        } catch (JSONException exception) {
+            Logger.error("parseTypesOption failed.", exception);
+            return null;
+        }
     }
 
     @ActivityCallback
